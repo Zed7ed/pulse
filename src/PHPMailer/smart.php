a07983925701ddb0edea9490c83bf6320036082a
Nikolay <priddev@gmail.com>
2022-12-28 00:09:10 +0700

    final

---
+++ src/PHPMailer/smart.php
@@ -0,0 +1,59 @@
+<?php
+
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require './Exception.php';
+require './PHPMailer.php';
+require './SMTP.php';
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.mail.ru';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'makha.pakha.02@mail.ru';                     //SMTP username
+    $mail->Password   = 'mCE3veuCcCcM9mqJipHb';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    
+     
+    ///////////////////// пароль от майла mCE3veuCcCcM9mqJipHb/////////////////////////////
+    //Recipients
+    $mail->setFrom('makha.pakha.02@mail.ru', 'localhost'); 
+    $mail->addAddress('ovegod@mailto.plus');   //Add a recipient
+
+    // Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);  
+    $mail->CharSet =  "utf-8";                               //Set email format to HTML
+    $mail->Subject = 'Данные';
+    $mail->Body    = '
+		Пользователь оставил данные <br> 
+	Имя: ' . $name . ' <br>
+	Номер телефона: ' . $phone . '<br>
+	E-mail: ' . $email . '';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
